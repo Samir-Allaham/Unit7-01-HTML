be50556416db70fcd54a791084c6b89aa061a884
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-18 16:13:18 +0000

    added =, indentations and spaces

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-//Connect "Guess" button to "random" function
+// Connect "Guess" button to "random" function
 document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let userGuess = 0
 
 function randomizer () {
-  //get number typed in by user and convert to an integer
-userGuess = document.getElementById('input').value
-userGuess = parseInt(userGuess)
+  // get number typed in by user and convert to an integer
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
 
-//generate random number between 1 and 6
-randomNumber = (Math.random() * 6) + 1
-randomNumber = parseInt(randomNumber)
+  // generate random number between 1 and 6
+  randomNumber = (Math.random() * 6) + 1
+  randomNumber = parseInt(randomNumber)
 
-//compare user's guess with random number
-if (userGuess == randomNumber){
-document.getElementById('answer').innerHTML =
-"You win!"
- }
+  // compare user's guess with random number
+  if (userGuess === randomNumber){
+    document.getElementById('answer').innerHTML =
+    'You win!'
+  }
 }
